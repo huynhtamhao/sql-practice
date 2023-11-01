6515ba3d82de1925ddd145a7b7c68009554af724
Huynh Tam Hao <huynhtamhao@gmail.com>
2023-11-01 20:53:28 +0700

    Update postgres-pagination.sql

--- postgres-pagination.sql
+++ postgres-pagination.sql
@@ -58,3 +58,25 @@ SELECT *
 FROM testT
 WHERE rowNum > (totalCount - 1)/10 * 10
 order by ID;
+
+
+----------- SQL name generate
+SELECT
+    arrays.firstnames[s.a % ARRAY_LENGTH(arrays.firstnames,1) + 1] AS firstname,
+    substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ' from s.a%26+1 for 1) AS middlename,
+    arrays.lastnames[s.a % ARRAY_LENGTH(arrays.lastnames,1) + 1] AS lastname
+FROM     generate_series(1,300) AS s(a) -- number of names to generate
+CROSS JOIN(
+    SELECT ARRAY[
+    'Adam','Bill','Bob','Calvin','Donald','Dwight','Frank','Fred','George','Howard',
+    'James','John','Jacob','Jack','Martin','Matthew','Max','Michael',
+    'Paul','Peter','Phil','Roland','Ronald','Samuel','Steve','Theo','Warren','William',
+    'Abigail','Alice','Allison','Amanda','Anne','Barbara','Betty','Carol','Cleo','Donna',
+    'Jane','Jennifer','Julie','Martha','Mary','Melissa','Patty','Sarah','Simone','Susan'
+    ] AS firstnames,
+    ARRAY[
+        'Matthews','Smith','Jones','Davis','Jacobson','Williams','Donaldson','Maxwell','Peterson','Stevens',
+        'Franklin','Washington','Jefferson','Adams','Jackson','Johnson','Lincoln','Grant','Fillmore','Harding','Taft',
+        'Truman','Nixon','Ford','Carter','Reagan','Bush','Clinton','Hancock'
+    ] AS lastnames
+) AS arrays
